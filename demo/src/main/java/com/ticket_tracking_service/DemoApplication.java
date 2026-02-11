24d07d490c061ec780c12bbe33c4b83b8db57e68
gaouravpatil <patilgaourav304@gmail.com>
2026-02-11 11:24:51 +0530

    dockerfile updated

---
+++ demo/src/main/java/com/ticket_tracking_service/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.ticket_tracking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableCaching
+@EnableScheduling
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
